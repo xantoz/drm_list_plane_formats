1ec7c34c4e3c14651070e3ed420184510912f168
Anton Kindestam <antonki@kth.se>
2018-04-21 15:30:31 +0200

    Also output encoders and connectors + some more things + more nicely adjusted output

--- drm_list_plane_formats.c
+++ drm_list_plane_formats.c
@@ -12,12 +12,29 @@
 
 #include <drm_fourcc.h>
 
+struct uint_string_map
+{
+    uint32_t uint;
+    char *name;
+};
+
+static const char *uint_string_map_find(uint32_t key,
+                                        const struct uint_string_map *map,
+                                        size_t len)
+{
+    for (unsigned i = 0; i < len; ++i) {
+        if (map[i].uint == key)
+            return map[i].name;
+    }
+
+    return "NOT FOUND";
+}
+
+#define FIND(key, x) (uint_string_map_find((key), (x), sizeof((x))/sizeof((x)[0])))
+
 static const char *fourcc_to_string(uint32_t fourcc)
 {
-    static const struct {
-        uint32_t format;
-        char *name;
-    } formats[] = {
+    static const struct uint_string_map formats[] = {
         {DRM_FORMAT_C8, "DRM_FORMAT_C8"},
         {DRM_FORMAT_GR88, "DRM_FORMAT_GR88"},
         {DRM_FORMAT_RGB332, "DRM_FORMAT_RGB332"},
@@ -79,20 +96,83 @@ static const char *fourcc_to_string(uint32_t fourcc)
         {DRM_FORMAT_YVU444, "DRM_FORMAT_YVU444"},
     };
 
-    for (unsigned i = 0; i < sizeof(formats)/sizeof(formats[0]); ++i) {
-        if (formats[i].format == fourcc)
-            return formats[i].name;
-    }
+    return FIND(fourcc, formats);
+}
 
-    return "NOT FOUND";
+static const char *encoder_type_to_string(uint32_t encoder_type)
+{
+    static const struct uint_string_map encoder_types[] = {
+        {DRM_MODE_ENCODER_NONE, "DRM_MODE_ENCODER_NONE"},
+        {DRM_MODE_ENCODER_DAC, "DRM_MODE_ENCODER_DAC"},
+        {DRM_MODE_ENCODER_TMDS, "DRM_MODE_ENCODER_TMDS"},
+        {DRM_MODE_ENCODER_LVDS, "DRM_MODE_ENCODER_LVDS"},
+        {DRM_MODE_ENCODER_TVDAC, "DRM_MODE_ENCODER_TVDAC"},
+        {DRM_MODE_ENCODER_VIRTUAL, "DRM_MODE_ENCODER_VIRTUAL"},
+        {DRM_MODE_ENCODER_DSI, "DRM_MODE_ENCODER_DSI"},
+        {DRM_MODE_ENCODER_DPMST, "DRM_MODE_ENCODER_DPMST"},
+        {DRM_MODE_ENCODER_DPI, "DRM_MODE_ENCODER_DPI"},
+    };
+
+    return FIND(encoder_type, encoder_types);
+}
+
+static const char *connector_type_to_string(uint32_t connector_type)
+{
+    static const struct uint_string_map connector_types[] = {
+        {DRM_MODE_CONNECTOR_Unknown, "DRM_MODE_CONNECTOR_Unknown"},
+        {DRM_MODE_CONNECTOR_VGA, "DRM_MODE_CONNECTOR_VGA"},
+        {DRM_MODE_CONNECTOR_DVII, "DRM_MODE_CONNECTOR_DVII"},
+        {DRM_MODE_CONNECTOR_DVID, "DRM_MODE_CONNECTOR_DVID"},
+        {DRM_MODE_CONNECTOR_DVIA, "DRM_MODE_CONNECTOR_DVIA"},
+        {DRM_MODE_CONNECTOR_Composite, "DRM_MODE_CONNECTOR_Composite"},
+        {DRM_MODE_CONNECTOR_SVIDEO, "DRM_MODE_CONNECTOR_SVIDEO"},
+        {DRM_MODE_CONNECTOR_LVDS, "DRM_MODE_CONNECTOR_LVDS"},
+        {DRM_MODE_CONNECTOR_Component, "DRM_MODE_CONNECTOR_Component"},
+        {DRM_MODE_CONNECTOR_9PinDIN, "DRM_MODE_CONNECTOR_9PinDIN"},
+        {DRM_MODE_CONNECTOR_DisplayPort, "DRM_MODE_CONNECTOR_DisplayPort"},
+        {DRM_MODE_CONNECTOR_HDMIA, "DRM_MODE_CONNECTOR_HDMIA"},
+        {DRM_MODE_CONNECTOR_HDMIB, "DRM_MODE_CONNECTOR_HDMIB"},
+        {DRM_MODE_CONNECTOR_TV, "DRM_MODE_CONNECTOR_TV"},
+        {DRM_MODE_CONNECTOR_eDP, "DRM_MODE_CONNECTOR_eDP"},
+        {DRM_MODE_CONNECTOR_VIRTUAL, "DRM_MODE_CONNECTOR_VIRTUAL"},
+        {DRM_MODE_CONNECTOR_DSI, "DRM_MODE_CONNECTOR_DSI"},
+        {DRM_MODE_CONNECTOR_DPI, "DRM_MODE_CONNECTOR_DPI"},
+    };
+
+    return FIND(connector_type, connector_types);
 }
 
 static const char *plane_type_to_string(uint32_t plane_type)
 {
     switch (plane_type) {
-        case DRM_PLANE_TYPE_OVERLAY:  return "DRM_PLANE_TYPE_OVERLAY";
-        case DRM_PLANE_TYPE_PRIMARY:  return "DRM_PLANE_TYPE_PRIMARY";
-        case DRM_PLANE_TYPE_CURSOR:   return "DRM_PLANE_TYPE_CURSOR";
+    case DRM_PLANE_TYPE_OVERLAY:  return "DRM_PLANE_TYPE_OVERLAY";
+    case DRM_PLANE_TYPE_PRIMARY:  return "DRM_PLANE_TYPE_PRIMARY";
+    case DRM_PLANE_TYPE_CURSOR:   return "DRM_PLANE_TYPE_CURSOR";
+    }
+
+    return "???";
+}
+
+static const char *drmModeConnection_to_string(drmModeConnection connection)
+{
+    switch (connection) {
+    case DRM_MODE_CONNECTED:         return "DRM_MODE_CONNECTED";
+    case DRM_MODE_DISCONNECTED:      return "DRM_MODE_DISCONNECTED";
+    case DRM_MODE_UNKNOWNCONNECTION: return "DRM_MODE_UNKNOWNCONNECTION";
+    }
+
+    return "???";
+}
+
+static const char *drmModeSubPixel_to_string(drmModeSubPixel sp)
+{
+    switch (sp) {
+    case DRM_MODE_SUBPIXEL_UNKNOWN:        return "DRM_MODE_SUBPIXEL_UNKNOWN";
+    case DRM_MODE_SUBPIXEL_HORIZONTAL_RGB: return "DRM_MODE_SUBPIXEL_HORIZONTAL_RGB";
+    case DRM_MODE_SUBPIXEL_HORIZONTAL_BGR: return "DRM_MODE_SUBPIXEL_HORIZONTAL_BGR";
+    case DRM_MODE_SUBPIXEL_VERTICAL_RGB:   return "DRM_MODE_SUBPIXEL_VERTICAL_RGB";
+    case DRM_MODE_SUBPIXEL_VERTICAL_BGR:   return "DRM_MODE_SUBPIXEL_VERTICAL_BGR";
+    case DRM_MODE_SUBPIXEL_NONE:           return "DRM_MODE_SUBPIXEL_NONE";
     }
 
     return "???";
@@ -109,12 +189,14 @@ static void list_planes(int fd)
 
     for (unsigned int i = 0; i < plane_res->count_planes; i++) {
         drmModePlane *drmplane = drmModeGetPlane(fd, plane_res->planes[i]);
+        if (NULL == drmplane)
+            continue;
         drmModeObjectProperties *properties =
             drmModeObjectGetProperties(fd,
                                        drmplane->plane_id,
                                        DRM_MODE_OBJECT_PLANE);
         drmModePropertyPtr prop = NULL;
-        uint64_t value = 0;
+        uint64_t value = 4000;
         for (unsigned j = 0; j < properties->count_props; ++j) {
             prop = drmModeGetProperty(fd, properties->props[j]);
             if (0 == strcasecmp("TYPE", prop->name)) {
@@ -125,11 +207,20 @@ static void list_planes(int fd)
             prop = NULL;
         }
         for (unsigned j = 0; j < drmplane->count_formats; ++j) {
-            printf("plane %d [%s] format: %#x %s\n",
+            char substr[128];
+            snprintf(substr, sizeof(substr), "[%s],", plane_type_to_string(value));
+            printf("plane %2u (crtc_id: %2u) %-25s format: %#x  %-25s",
                    drmplane->plane_id,
-                   plane_type_to_string(value),
+                   drmplane->crtc_id,
+                   substr,
                    drmplane->formats[j],
                    fourcc_to_string(drmplane->formats[j]));
+            printf("%s", " | possible_crtcs: [");
+            for (unsigned k = 0; k < sizeof(uint32_t); ++k) {
+                if (drmplane->possible_crtcs & (uint32_t)(1 << k))
+                    printf(" %u ", k);
+            }
+            puts("]");
         }
         drmModeFreeProperty(prop);
         prop = NULL;
@@ -141,6 +232,85 @@ static void list_planes(int fd)
 
     if (plane_res)
         drmModeFreePlaneResources(plane_res);
+}
+
+static void list_encoders(int fd)
+{
+    drmModeRes *res = drmModeGetResources(fd);
+
+    if (res == NULL) {
+        fputs("ERROR: Could not get resources\n", stderr);
+        abort();
+    }
+
+    for (unsigned i = 0; i < res->count_encoders; ++i) {
+        drmModeEncoder *enc = drmModeGetEncoder(fd, res->encoders[i]);
+        if (NULL == enc)
+            continue;
+
+        char substr[128];
+        snprintf(substr, sizeof(substr), "[%s],", encoder_type_to_string(enc->encoder_type));
+        printf("encoder_id: %2u %-27s crtc_id: %2u",
+               enc->encoder_id, substr, enc->crtc_id);
+        printf("%s", " | possible_crtcs: [");
+        for (unsigned j = 0; j < sizeof(uint32_t); ++j) {
+            if (enc->possible_crtcs & (uint32_t)(1 << j))
+                printf(" %d ", j);
+        }
+        putchar(']');
+
+        printf("%s", " | possible_clones: [");
+        for (unsigned j = 0; j < sizeof(uint32_t); ++j) {
+            if (enc->possible_clones & (uint32_t)(1 << j))
+                printf(" %d ", j);
+        }
+        putchar(']');
+
+        puts("");
+
+        drmModeFreeEncoder(enc);
+    }
+
+    if (res)
+        drmModeFreeResources(res);
+}
+
+static void list_connectors(int fd)
+{
+    drmModeRes *res = drmModeGetResources(fd);
+
+    if (res == NULL) {
+        fputs("ERROR: Could not get resources\n", stderr);
+        abort();
+    }
+
+    for (unsigned i = 0; i < res->count_connectors; ++i) {
+        drmModeConnector *conn = drmModeGetConnector(fd, res->connectors[i]);
+        if (NULL == conn)
+            continue;
+
+        char substr[128];
+        snprintf(substr, sizeof(substr), "[%s #%u],",
+                 connector_type_to_string(conn->connector_type),
+                 conn->connector_type_id);
+        char info[128];
+        snprintf(info, sizeof(info), "<%ummX%umm>", conn->mmWidth, conn->mmHeight);
+        char subpixel[128];
+        snprintf(subpixel, sizeof(subpixel), "[%s],", drmModeSubPixel_to_string(conn->subpixel));
+        printf("connector_id: %2u %15s %-36s %-35s encoder_id: %2u, connection: %s\n",
+               conn->connector_id,
+               info,
+               substr,
+               subpixel,
+               conn->encoder_id,
+               drmModeConnection_to_string(conn->connection));
+        // TODO: modes
+
+        drmModeFreeConnector(conn);
+    }
+
+    if (res)
+        drmModeFreeResources(res);
 }
 
 int main()
@@ -169,6 +339,12 @@ int main()
 
     puts("\n--WITH UNIVERSAL PLANES--");
     list_planes(fd);
+
+    puts("\n--ENCODERS--");
+    list_encoders(fd);
+
+    puts("\n--CONNECTORS--");
+    list_connectors(fd);
 
     return 0;
 }
